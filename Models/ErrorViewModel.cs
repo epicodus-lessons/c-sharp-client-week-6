1fbc27e4967a96be84fb8158944b1d47441595aa
Travis Scott <travisty12@gmail.com>
2020-03-04 15:41:18 -0800

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace c_sharp_client_week_6.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
