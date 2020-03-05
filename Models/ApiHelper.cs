c2ed6e87a4de2e92b62f7b65c76406f1a52b3585
Travis Scott <travisty12@gmail.com>
2020-03-05 12:53:36 -0800

    add delete method to apihelper

--- Models/ApiHelper.cs
+++ Models/ApiHelper.cs
@@ -38,5 +38,13 @@ namespace CretaceousClient.Models
       request.AddJsonBody(newAnimal);
       var response = await client.ExecuteTaskAsync(request);
     }
+
+    public static async Task Delete(int id)
+    {
+      RestClient client = new RestClient("http://localhost:5004/api");
+      RestRequest request = new RestRequest($"animals/{id}", Method.DELETE);
+      request.AddHeader("Content-Type", "application/json");
+      var response = await client.ExecuteTaskAsync(request);
+    }
   }
 }
